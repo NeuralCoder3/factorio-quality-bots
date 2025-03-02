f091a7de98714d40752d6afd22fa0a283c93f329
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2025-03-02 13:38:18 -0800

    factoriopedia description of payload size

--- data-updates.lua
+++ data-updates.lua
@@ -1,13 +1,13 @@
 local common = require("common")
 
-for _, q in pairs(data.raw["quality"]) do
-  local k = common.padleft(q.level, 2, "0").."-"..q.name
-  local v = 1 + settings.startup["robot-payload-size-multiplier"].value * q.level
+local robots = {}
+for _, name in pairs(common.robot_names) do
+  if not data.raw[name] then goto continue end
+  local quality_string = ""
+  for _, q in pairs(data.raw["quality"]) do
+    local k = common.padleft(q.level, 2, "0").."-"..q.name
+    local v = 1 + settings.startup["robot-payload-size-multiplier"].value * q.level
 
-
-  local robots = {}
-  for _, name in pairs(common.robot_names) do
-    if not data.raw[name] then goto continue end
     local robot = table.deepcopy(data.raw[name][name])
     robot.name = k.."-"..robot.name
     robot.max_payload_size = robot.max_payload_size * v
@@ -15,8 +15,10 @@ for _, q in pairs(data.raw["quality"]) do
     robot.localised_description = {"entity-description."..name}
     robot.placeable_by = {item = name, count = 1, quality = k}
     table.insert(robots, robot)
-    ::continue::
-  end
 
-  data:extend(robots)
+    quality_string = quality_string..q.name.." -> "..robot.max_payload_size.."\n"
+  end
+  data.raw[name][name].factoriopedia_description = {"",quality_string}
+  ::continue::
 end
+data:extend(robots)
